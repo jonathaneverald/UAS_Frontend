f4560e1b01e88900132d44b11953eabb2eb753d4
ASUS <jonathaneverald@gmail.com>
2021-11-24 21:30:24 +0700

    ugd

--- src/components/TodoList/List.vue
+++ src/components/TodoList/List.vue
@@ -1,6 +1,17 @@
 <template>
     <v-main class="list">
         <h3 class="text-h3 font-weight-medium mb-5">To Do List</h3>
+        <v-dialog v-model="dialogDelete" max-width="500px">
+          <v-card>
+            <v-card-title class="text-h5">Are you sure you want to delete this item?</v-card-title>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
+              <v-btn color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>
+              <v-spacer></v-spacer>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
         <v-card>
             <v-card-title>
                 <v-text-field
@@ -53,17 +64,6 @@
                 </v-card-actions>
             </v-card>
         </v-dialog>
-        <v-dialog v-model="dialogDelete" max-width="500px">
-          <v-card>
-            <v-card-title class="text-h5">Are you sure you want to delete this item?</v-card-title>
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
-              <v-btn color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>
-              <v-spacer></v-spacer>
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
     </v-main>
 </template>
 
@@ -106,11 +106,11 @@ export default {
             ],
             formTodo: { task: null, priority: null, note: null },
             watch: {
-                dialog (val) {
-                    val || this.close()
-                },
                 dialogDelete (val) {
                     val || this.closeDelete()
+                },
+                dialog (val) {
+                    val || this.close()
                 },
             },
         };
@@ -126,43 +126,37 @@ export default {
             }
             this.close();
         },
-        
-        cancel() {
+	    cancel() {
             this.resetForm();
             this.dialog = false;
         },
-
-        close() {
-            this.dialog = false;
-            this.resetForm();
-            this.editedIndex = -1;
-        },
-
-        closeDelete() {
-            this.dialogDelete = false;
-            this.resetForm();
-            this.editedIndex = -1;
-        },
-
         resetForm() {
             this.formTodo = { task: null, priority: null, note: null };
         },
-
+        editItem(item) {
+            this.editedIndex = this.todos.indexOf(item);
+            this.formTodo = Object.assign({}, item);
+            this.dialog = true;
+        },
         deleteItem(item) {
             this.editedIndex = this.todos.indexOf(item);
             this.formTodo = Object.assign({}, item);
             this.dialogDelete = true;
         },
-
         deleteItemConfirm() {
             this.todos.splice(this.editedIndex, 1);
             this.closeDelete();
         },
-
-        editItem(item) {
-            this.editedIndex = this.todos.indexOf(item);
-            this.formTodo = Object.assign({}, item);
-            this.dialog = true;
+	    
+        close() {
+            this.dialog = false;
+            this.resetForm();
+            this.editedIndex = -1;
+        },
+        closeDelete() {
+            this.dialogDelete = false;
+            this.resetForm();
+            this.editedIndex = -1;
         },
     },
 };
